aa114c93d9a09892c21432881baf25835277dd3c
François Lecomte <francois.lecomte-ext9sgcib.com>
2015-05-11 17:49:56 +0200

    junitparams

--- src/test/java/com/foobarqix/FooBarQixTest.java
+++ src/test/java/com/foobarqix/FooBarQixTest.java
@@ -1,10 +1,14 @@
 package com.foobarqix;
 
 import static org.junit.Assert.assertEquals;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class FooBarQixTest {
 
 	private FooBarQix fooBarQix;
@@ -15,38 +19,49 @@ public class FooBarQixTest {
 	}
 
 	@Test
-	public void should_return_string_conversion_when_not_divisible_nor_contains_special_number() {
-		assertEquals("8", fooBarQix.convertToText(8));
-		assertEquals("16", fooBarQix.convertToText(16));
+	@Parameters({ "8, 8", //
+			"16, 16" })
+	public void should_return_string_conversion_when_not_divisible_nor_contains_special_number(int number, String text) {
+		assertEquals(text, fooBarQix.convertToText(number));
 	}
 
 	@Test
-	public void should_return_matching_text_when_divisible_by_one_special_number_only() {
-		assertEquals("Foo", fooBarQix.convertToText(9));
-		assertEquals("Bar", fooBarQix.convertToText(10));
-		assertEquals("Qix", fooBarQix.convertToText(14));
+	@Parameters({ "9, Foo", //
+			"10, Bar", //
+			"14, Qix" })
+	public void should_return_matching_text_when_divisible_by_one_special_number_only(int number, String text) {
+		assertEquals(text, fooBarQix.convertToText(number));
 	}
 
 	@Test
-	public void should_return_matching_text_when_contains_one_special_number_only() {
-		assertEquals("Foo", fooBarQix.convertToText(13));
-		assertEquals("Bar", fooBarQix.convertToText(52));
-		assertEquals("Qix", fooBarQix.convertToText(71));
+	@Parameters({ "13, Foo", //
+			"52, Bar", //
+			"71, Qix" })
+	public void should_return_matching_text_when_contains_one_special_number_only(int number, String text) {
+		assertEquals(text, fooBarQix.convertToText(number));
 	}
 
 	@Test
-	public void should_return_text_ordered_by_divisor_occurences_when_divisible_by_several_special_numbers() {
-		assertEquals("FooQix", fooBarQix.convertToText(21));
+	@Parameters({ "21, FooQix" })
+	public void should_return_text_ordered_by_divisor_occurences_when_divisible_by_several_special_numbers(int number,
+			String text) {
+		assertEquals(text, fooBarQix.convertToText(number));
 	}
 
 	@Test
-	public void should_return_text_ordered_by_special_numbers_occurences_when_contains_several_special_numbers() {
-		assertEquals("BarFoo", fooBarQix.convertToText(53));
+	@Parameters({ "15, FooBarBar", //
+			"33, FooFooFoo", //
+			"35, BarQixFooBar", //
+			"51, FooBar" })
+	public void should_return_text_ordered_when_divisible_by_special_numbers_and_contains_special_numbers(int number,
+			String text) {
+		assertEquals(text, fooBarQix.convertToText(number));
 	}
 
 	@Test
-	public void should_return_text_ordered_when_divisible_by_special_numbers_and_contains_special_numbers() {
-		assertEquals("FooFooFoo", fooBarQix.convertToText(33));
-		assertEquals("BarQixFooBar", fooBarQix.convertToText(35));
+	@Parameters({ "53, BarFoo" })
+	public void should_return_text_ordered_by_special_numbers_occurences_when_contains_several_special_numbers(
+			int number, String text) {
+		assertEquals(text, fooBarQix.convertToText(number));
 	}
 }
