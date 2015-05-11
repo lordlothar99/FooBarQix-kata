6f12193265ec2a9643532f9696a7ebaf7f27c1e4
François Lecomte <francois.lecomte-ext9sgcib.com>
2015-05-11 17:22:46 +0200

    initial

---
+++ src/main/java/com/foobarqix/FooBarQix.java
@@ -0,0 +1,11 @@
+package com.foobarqix;
+
+public class FooBarQix {
+
+	public static void main(String[] args) {
+		new FooBarQix().displayNumbers();
+	}
+
+	private void displayNumbers() {
+	}
+}
---
+++ src/test/java/com/foobarqix/FooBarQixTest.java
@@ -0,0 +1,4 @@
+package com.foobarqix;
+
+public class FooBarQixTest {
+}
