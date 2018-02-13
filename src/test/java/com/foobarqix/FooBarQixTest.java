8a8ecbf8bb2f0ef00238bb7eedcc06f866c759d8
François Lecomte <francois.lecomte@gmail.com>
2018-02-13 16:00:15 +0100

    refactoring : increase comprehensive testing

--- src/test/java/com/foobarqix/FooBarQixTest.java
+++ src/test/java/com/foobarqix/FooBarQixTest.java
@@ -13,57 +13,72 @@ import junitparams.Parameters;
 @RunWith(JUnitParamsRunner.class)
 public class FooBarQixTest {
 
-	private FooBarQix fooBarQix;
+    private FooBarQix fooBarQix;
 
-	@Before
-	public void setup() {
-		fooBarQix = new FooBarQix();
-	}
+    @Before
+    public void setup() {
+        fooBarQix = new FooBarQix();
+    }
 
-	@Test
-	@Parameters({ "8, 8", //
-			"16, 16" })
-	public void should_return_number_as_text_when_not_divisible_nor_contains_special_number(int number, String text) {
-		assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
-	}
+    @Test
+    @Parameters({"8, 8", //
+            "16, 16"})
+    public void should_return_number_as_text_when_not_divisible_nor_contains_special_number(
+            int number, String text) {
+        assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
+    }
 
-	@Test
-	@Parameters({ "9, Foo", //
-			"10, Bar", //
-			"14, Qix" })
-	public void should_return_matching_text_when_divisible_by_one_special_number_only(int number, String text) {
-		assertEquals(text, fooBarQix.convertToText(number));
-	}
+    @Test
+    public void should_return_Foo_when_divisible_by_3() {
+        assertThat(fooBarQix.convertToText(9)).isEqualTo("Foo");
+    }
 
-	@Test
-	@Parameters({ "13, Foo", //
-			"52, Bar", //
-			"71, Qix" })
-	public void should_return_matching_text_when_contains_one_special_number_only(int number, String text) {
-		assertEquals(text, fooBarQix.convertToText(number));
-	}
+    @Test
+    public void should_return_Bar_when_divisible_by_5() {
+        assertThat(fooBarQix.convertToText(10)).isEqualTo("Bar");
+    }
 
-	@Test
-	@Parameters({ "21, FooQix" })
-	public void should_return_text_ordered_by_divisor_occurences_when_divisible_by_several_special_numbers(int number,
-			String text) {
-		assertEquals(text, fooBarQix.convertToText(number));
-	}
+    @Test
+    public void should_return_Qix_when_divisible_by_7() {
+        assertThat(fooBarQix.convertToText(14)).isEqualTo("Qix");
+    }
 
-	@Test
-	@Parameters({ "15, FooBarBar", //
-			"33, FooFooFoo", //
-			"35, BarQixFooBar", //
-			"51, FooBar" })
-	public void should_return_text_ordered_when_divisible_by_special_numbers_and_contains_special_numbers(int number,
-			String text) {
-		assertEquals(text, fooBarQix.convertToText(number));
-	}
+    @Test
+    public void should_return_Foo_when_contains_3() {
+        assertThat(fooBarQix.convertToText(13)).isEqualTo("Foo");
+    }
 
-	@Test
-	@Parameters({ "53, BarFoo" })
-	public void should_return_text_ordered_by_special_numbers_occurences_when_contains_several_special_numbers(
-			int number, String text) {
-		assertEquals(text, fooBarQix.convertToText(number));
-	}
+    @Test
+    public void should_return_Bar_when_contains_5() {
+        assertThat(fooBarQix.convertToText(52)).isEqualTo("Bar");
+    }
+
+    @Test
+    public void should_return_Qix_when_contains_7() {
+        assertThat(fooBarQix.convertToText(71)).isEqualTo("Qix");
+    }
+
+    @Test
+    @Parameters({"21, FooQix"})
+    public void should_return_text_ordered_by_divisor_occurences_when_divisible_by_several_special_numbers(
+            int number, String text) {
+        assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
+    }
+
+    @Test
+    @Parameters({"15, FooBarBar", //
+            "33, FooFooFoo", //
+            "35, BarQixFooBar", //
+            "51, FooBar"})
+    public void should_return_text_ordered_when_divisible_by_special_numbers_and_contains_special_numbers(
+            int number, String text) {
+        assertEquals(text, fooBarQix.convertToText(number));
+    }
+
+    @Test
+    @Parameters({"53, BarFoo"})
+    public void should_return_text_ordered_by_special_numbers_occurences_when_contains_several_special_numbers(
+            int number, String text) {
+        assertEquals(text, fooBarQix.convertToText(number));
+    }
 }
