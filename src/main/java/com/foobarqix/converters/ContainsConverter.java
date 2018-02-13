d7fb727359809e7bcb6902521ca44d5546522917
François Lecomte <francois.lecomte@gmail.com>
2018-02-13 16:16:22 +0100

    refactor (import static)

--- src/main/java/com/foobarqix/converters/ContainsConverter.java
+++ src/main/java/com/foobarqix/converters/ContainsConverter.java
@@ -1,26 +1,27 @@
 package com.foobarqix.converters;
 
 import static com.foobarqix.SpecialNumber.getByCharacter;
+import static java.lang.String.valueOf;
 
 import com.foobarqix.SpecialNumber;
 
 public class ContainsConverter implements NumberConverter {
 
-	@Override
-	public String convertToText(int number) {
-		String text = "";
-		for (char character : toChars(number)) {
-			text += convertToText(character);
-		}
-		return text;
-	}
+    @Override
+    public String convertToText(int number) {
+        String text = "";
+        for (char character : toChars(number)) {
+            text += convertToText(character);
+        }
+        return text;
+    }
 
-	private String convertToText(char character) {
-		SpecialNumber specialNumber = getByCharacter(character);
-		return specialNumber != null ? specialNumber.getText() : "";
-	}
+    private String convertToText(char character) {
+        SpecialNumber specialNumber = getByCharacter(character);
+        return specialNumber != null ? specialNumber.getText() : "";
+    }
 
-	private char[] toChars(int number) {
-		return String.valueOf(number).toCharArray();
-	}
+    private char[] toChars(int number) {
+        return valueOf(number).toCharArray();
+    }
 }
