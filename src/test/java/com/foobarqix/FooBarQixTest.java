9ba7cbb939e3a4c3a212142528c5bb177df01ffc
François Lecomte <francois.lecomte@gmail.com>
2018-02-13 16:14:22 +0100

    refactoring : increase comprehensive testing

--- src/test/java/com/foobarqix/FooBarQixTest.java
+++ src/test/java/com/foobarqix/FooBarQixTest.java
@@ -29,40 +29,62 @@ public class FooBarQixTest {
     }
 
     @Test
-    public void should_return_Foo_when_divisible_by_3() {
-        assertThat(fooBarQix.convertToText(9)).isEqualTo("Foo");
+    @Parameters({"9, Foo", //
+            "12, Foo"})
+    public void should_return_Foo_when_divisible_by_3(int number, String text) {
+        assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
     }
 
     @Test
-    public void should_return_Bar_when_divisible_by_5() {
-        assertThat(fooBarQix.convertToText(10)).isEqualTo("Bar");
+    @Parameters({"10, Bar", //
+            "20, Bar"})
+    public void should_return_Bar_when_divisible_by_5(int number, String text) {
+        assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
     }
 
     @Test
-    public void should_return_Qix_when_divisible_by_7() {
-        assertThat(fooBarQix.convertToText(14)).isEqualTo("Qix");
+    @Parameters({"14, Qix", //
+            "28, Qix"})
+    public void should_return_Qix_when_divisible_by_7(int number, String text) {
+        assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
     }
 
     @Test
-    public void should_return_Foo_when_contains_3() {
-        assertThat(fooBarQix.convertToText(13)).isEqualTo("Foo");
+    @Parameters({"13, Foo", //
+            "23, Foo"})
+    public void should_return_Foo_when_contains_3(int number, String text) {
+        assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
     }
 
     @Test
-    public void should_return_Bar_when_contains_5() {
-        assertThat(fooBarQix.convertToText(52)).isEqualTo("Bar");
+    @Parameters({"52, Bar", //
+            "58, Bar"})
+    public void should_return_Bar_when_contains_5(int number, String text) {
+        assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
     }
 
     @Test
-    public void should_return_Qix_when_contains_7() {
-        assertThat(fooBarQix.convertToText(71)).isEqualTo("Qix");
+    @Parameters({"71, Qix", //
+            "79, Qix"})
+    public void should_return_Qix_when_contains_7(int number, String text) {
+        assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
     }
 
     @Test
-    @Parameters({"21, FooQix"})
-    public void should_return_text_ordered_by_divisor_occurences_when_divisible_by_several_special_numbers(
+    @Parameters({"21, FooQix", //
+            "140, BarQix", //
+            "60, FooBar"})
+    public void should_return_text_ordered_ascending_when_divisible_by_several_special_numbers(
             int number, String text) {
         assertThat(fooBarQix.convertToText(number)).isEqualTo(text);
+    }
+
+    @Test
+    @Parameters({"53, BarFoo", //
+            "157, BarQix"})
+    public void should_return_text_ordered_by_occurence_when_contains_several_special_numbers(
+            int number, String text) {
+        assertEquals(text, fooBarQix.convertToText(number));
     }
 
     @Test
@@ -70,14 +92,7 @@ public class FooBarQixTest {
             "33, FooFooFoo", //
             "35, BarQixFooBar", //
             "51, FooBar"})
-    public void should_return_text_ordered_when_divisible_by_special_numbers_and_contains_special_numbers(
-            int number, String text) {
-        assertEquals(text, fooBarQix.convertToText(number));
-    }
-
-    @Test
-    @Parameters({"53, BarFoo"})
-    public void should_return_text_ordered_by_special_numbers_occurences_when_contains_several_special_numbers(
+    public void should_return_text_with_precendence_on_divisor_when_divisible_and_contains_special_numbers(
             int number, String text) {
         assertEquals(text, fooBarQix.convertToText(number));
     }
